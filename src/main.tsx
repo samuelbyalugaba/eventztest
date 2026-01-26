a8aa75d2bc109785ddd88e5dd0c4f1a6ec0332c3
samuelbyalugaba <samuelbyalugaba@users.noreply.github.com>
2026-01-26 17:35:04 +0300

    fix: install and configure tailwindcss v4

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 
   import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./styles/globals.css";
 import "./index.css";
 import { registerServiceWorker } from "./utils/registerSW";
 import { ErrorBoundary } from "./components/ErrorBoundary";
